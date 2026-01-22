eb714cdb924f243e31620d6b4cf747e8c6620ad9
cristiandecker <cristianc.cortesb@uqvirtual.edu.co>
2026-01-22 14:39:16 -0500

    chore: stop tracking .next (build output)

--- .next/dev/server/chunks/ssr/_next-internal_server_app_dashboard_audits_page_actions_9592ccd0.js
+++
@@ -1,7 +0,0 @@
-module.exports = [
-"[project]/.next-internal/server/app/dashboard/audits/page/actions.js [app-rsc] (server actions loader, ecmascript)", ((__turbopack_context__, module, exports) => {
-
-}),
-];
-
-//# sourceMappingURL=_next-internal_server_app_dashboard_audits_page_actions_9592ccd0.js.map
--- .next/dev/server/chunks/ssr/_next-internal_server_app_dashboard_roles_page_actions_3d8b72ef.js
+++
@@ -1,7 +0,0 @@
-module.exports = [
-"[project]/.next-internal/server/app/dashboard/roles/page/actions.js [app-rsc] (server actions loader, ecmascript)", ((__turbopack_context__, module, exports) => {
-
-}),
-];
-
-//# sourceMappingURL=_next-internal_server_app_dashboard_roles_page_actions_3d8b72ef.js.map
